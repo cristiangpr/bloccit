2cc01476175fe312392ab4e70bd3a66168024307
Cristian Perez <cristiangp777@gmail.com>
2019-02-20 14:23:07 -0800

    temp

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -16,8 +16,41 @@ describe("routes : static", () => {
 
 //#4
         done();
+
+
       });
     });
+
+
+  });
+  describe("GET /marco", () => {
+
+//#2
+    it("should return status code 200", (done) => {
+
+//#3
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+
+//#4
+        done();
+
+
+      });
+    });
+    it("should contain the string 'polo' in response", (done) => {
+
+//#3
+      request.get(base, (err, res, body) => {
+        expect(res.body).toBe("polo");
+
+//#4
+        done();
+
+
+      });
+    });
+
 
   });
 });
