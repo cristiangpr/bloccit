73c6b8ea5bc86c990cbb7786067ff3bd8afdd016
Cristian Perez <cristiangp777@gmail.com>
2019-02-22 13:47:00 -0800

    weird

--- spec/integration/topics_spec.js
+++ spec/integration/topics_spec.js
@@ -1,6 +1,8 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/topics/";
+const sequelize = require("../../src/db/models/index").sequelize;
+const Topic = require("../../src/db/models").Topic;
 
 describe("routes : topics", () => {
 
