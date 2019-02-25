c38c3067afe7657411f7bdc09e67befcaaa24c24
Cristian Perez <cristiangp777@gmail.com>
2019-02-25 15:14:46 -0800

    update working

--- spec/integration/topics_spec.js
+++ spec/integration/topics_spec.js
@@ -138,4 +138,31 @@ describe("GET /topics/:id/edit", () => {
     });
 
   });
+  describe("POST /topics/:id/update", () => {
+
+      it("should update the topic with the given values", (done) => {
+         const options = {
+            url: `${base}${this.topic.id}/update`,
+            form: {
+              title: "JavaScript Frameworks",
+              description: "There are a lot of them"
+            }
+          };
+ //#1
+          request.post(options,
+            (err, res, body) => {
+
+            expect(err).toBeNull();
+ //#2
+            Topic.findOne({
+              where: { id: this.topic.id }
+            })
+            .then((topic) => {
+              expect(topic.title).toBe("JavaScript Frameworks");
+              done();
+            });
+          });
+      });
+
+    });  
 });
