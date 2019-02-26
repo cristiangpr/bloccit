c49f589243a3af7f6e4396c09294f38655ad6ec4
Cristian Perez <cristiangp777@gmail.com>
2019-02-25 18:33:41 -0800

    temp

--- src/controllers/advertisementController.js
+++ src/controllers/advertisementController.js
@@ -11,5 +11,67 @@ module.exports = {
             res.render("advertisements/index", {advertisements});
           }
         })
-  }
-}
+  },
+  new(req, res, next){
+    res.render("advertisements/new");
+  },
+
+  create(req, res, next){
+    let newAdvertisement = {
+      title: req.body.title,
+      description: req.body.description
+    };
+    advertisementQueries.addAdvertisement(newAdvertisement, (err, advertisement) => {
+      if(err){
+        res.redirect(500, "/advertisements/new");
+      } else {
+        res.redirect(303, `/advertisements/${advertisement.id}`);
+      }
+    });
+  },
+  show(req, res, next){
+
+//#1
+     advertisementQueries.getAdvertisement(req.params.id, (err, topic) => {
+
+//#2
+       if(err || advertisement == null){
+         res.redirect(404, "/");
+       } else {
+         res.render("advertisements/show", {advertisement});
+       }
+     });
+   },
+
+   destroy(req, res, next){
+    advertisementQueries.deleteAdvertisement(req.params.id, (err, advertisement) => {
+      if(err){
+        res.redirect(500, `/advertisement/${advertisement.id}`)
+      } else {
+        res.redirect(303, "/advertisements")
+      }
+    });
+  },
+  edit(req, res, next){
+       advertisementQueries.getAdvertisement(req.params.id, (err, advertisement) => {
+         if(err || advertisement == null){
+           res.redirect(404, "/");
+         } else {
+           res.render("advertisements/edit", {advertisement});
+         }
+       });
+     },
+     update(req, res, next){
+
+    //#1
+         advertisementQueries.updatedAdvertisement(req.params.id, req.body, (err, advertisement) => {
+
+    //#2
+           if(err || advertisement == null){
+             res.redirect(404, `/advertisements/${req.params.id}/edit`);
+           } else {
+             res.redirect(`/advertisements/${advertisement.id}`);
+           }
+         });
+       }
+     }
