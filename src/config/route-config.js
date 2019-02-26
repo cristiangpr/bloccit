b656bed34a1cd3ea1bb3e2741d85cff3196faced
Cristian Perez <cristiangp777@gmail.com>
2019-02-26 15:35:53 -0800

    halfway

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -2,10 +2,12 @@ module.exports = {
   init(app){
     const staticRoutes = require("../routes/static");
     const topicRoutes = require("../routes/topics");
-    const advertisementRoutes = require("../routes/advertisements")
+    const advertisementRoutes = require("../routes/advertisements");
+    const postRoutes = require("../routes/posts");
 
     app.use(staticRoutes);
     app.use(topicRoutes);
     app.use(advertisementRoutes);
+    app.use(postRoutes);
   }
 }
