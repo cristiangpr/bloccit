5fa17584b418f7c7b077bbed8d20c8a48c7b6640
Cristian Perez <cristiangp777@gmail.com>
2019-03-07 13:22:18 -0800

    sign up working

--- src/routes/validation.js
+++ src/routes/validation.js
@@ -28,7 +28,7 @@ module.exports = {
     if(req.method === "POST") {
 
 //#2
-    
+
       req.checkBody("title", "must be at least 5 characters in length").isLength({min: 5});
       req.checkBody("description", "must be at least 10 characters in length").isLength({min: 10});
     }
@@ -41,6 +41,24 @@ module.exports = {
 //#4
       req.flash("error", errors);
       return res.redirect(303, req.headers.referer)
+    } else {
+      return next();
+    }
+  },
+  validateUsers(req, res, next) {
+    if(req.method === "POST") {
+
+// #1
+      req.checkBody("email", "must be valid").isEmail();
+      req.checkBody("password", "must be at least 6 characters in length").isLength({min: 6})
+      req.checkBody("passwordConfirmation", "must match password provided").optional().matches(req.body.password);
+    }
+
+    const errors = req.validationErrors();
+
+    if (errors) {
+      req.flash("error", errors);
+      return res.redirect(req.headers.referer);
     } else {
       return next();
     }
