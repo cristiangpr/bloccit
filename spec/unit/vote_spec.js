293a8a0ec1dd8e12a3eebef71e5bdf4be9eb4de2
Cristian Perez <cristiangp777@gmail.com>
2019-03-09 15:17:51 -0800

    assignment 14 completed

--- spec/unit/vote_spec.js
+++ spec/unit/vote_spec.js
@@ -152,7 +152,7 @@ describe("Vote", () => {
         .catch((err) => {
 
           expect(err.message).toContain("Validation error");
-        
+
           done();
 
         })
@@ -217,6 +217,47 @@ describe("Vote", () => {
         });
       });
 
-    });//suits will begin here
+    });//
+    describe("#hasUpvoteFor()", ()=>{
+    it("should return true if user has upvote", (done)=>{
+      Vote.create({
+        value: 1,
+        userId: this.user.id,
+        postId: this.post.id
+      })
+      .then((vote)=>{
+        vote.postId.hasUpvoteFor()
+        .then((associatedVote)=>{
+          expect(this.vote).toBe(true)
+          done()
+        })
+      })
+      .catch((err)=>{
+        console.log(err)
+        done()
+      })
+    })
+  })
+
+  describe("#hasDownvoteFor()", ()=>{
+    it("should return true if user has downvote", (done)=>{
+      Vote.create({
+        value: -1,
+        userId: this.user.id,
+        postId: this.post.id
+      })
+      .then((vote)=>{
+        vote.postId.hasDownvoteFor()
+        .then((associatedVote)=>{
+          expect(this.vote).toBe(true)
+          done()
+        })
+      })
+      .catch((err)=>{
+        console.log(err)
+        done()
+      })
+    })
+  })
 
 });
