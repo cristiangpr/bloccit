a9cd63c8ab1fe12109bbc4af94cb879e9c62cc0d
Cristian Perez <cristiangp777@gmail.com>
2019-03-09 11:36:58 -0800

    temp

---
+++ spec/unit/vote_spec.js
@@ -0,0 +1,70 @@
+// #1
+const sequelize = require("../../src/db/models/index").sequelize;
+const Topic = require("../../src/db/models").Topic;
+const Post = require("../../src/db/models").Post;
+const Comment = require("../../src/db/models").Comment;
+const User = require("../../src/db/models").User;
+const Vote = require("../../src/db/models").Vote;
+
+describe("Vote", () => {
+
+  beforeEach((done) => {
+ // #2
+    this.user;
+    this.topic;
+    this.post;
+    this.vote;
+
+ // #3
+    sequelize.sync({force: true}).then((res) => {
+
+      User.create({
+        email: "starman@tesla.com",
+        password: "Trekkie4lyfe"
+      })
+      .then((res) => {
+        this.user = res;
+
+        Topic.create({
+          title: "Expeditions to Alpha Centauri",
+          description: "A compilation of reports from recent visits to the star system.",
+          posts: [{
+            title: "My first visit to Proxima Centauri b",
+            body: "I saw some rocks.",
+            userId: this.user.id
+          }]
+        }, {
+          include: {
+            model: Post,
+            as: "posts"
+          }
+        })
+        .then((res) => {
+          this.topic = res;
+          this.post = this.topic.posts[0];
+
+          Comment.create({
+            body: "ay caramba!!!!!",
+            userId: this.user.id,
+            postId: this.post.id
+          })
+          .then((res) => {
+            this.comment = res;
+            done();
+          })
+          .catch((err) => {
+            console.log(err);
+            done();
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          done();
+        });
+      });
+    });
+  });
+
+  //suits will begin here
+
+});
