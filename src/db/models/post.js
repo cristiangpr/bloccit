25e14785dcbcf2901871c7a06af545bfce9756ad
Cristian Perez <cristiangp777@gmail.com>
2019-03-19 18:13:53 -0700

    assignment 15 completed

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -57,6 +57,13 @@ userId: {
       postId: post.id
     });
   });
+  Post.afterCreate((post, callback) => {
+     return models.Vote.create({
+       value: 1,
+       userId: post.userId,
+       postId: post.id
+     });
+   });
   };
 
   Post.prototype.getPoints = function(){
